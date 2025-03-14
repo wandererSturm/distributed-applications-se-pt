fe556b1caa1e168bbb42a8e3e470cdd615281da6
wandererSturm <wanderer.sturm@gmail.com>
2025-03-14 19:22:39 +0200

    Change in migrations

---
+++ course-work/Implementations/PlatformService/PS.WebApiService/Migrations/20250307182705_OperatingSystem_2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PS.WebApiService.Migrations
+{
+    /// <inheritdoc />
+    public partial class OperatingSystem_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OperatingSystemr",
+                table: "Platforms",
+                newName: "OperatingSystem");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OperatingSystem",
+                table: "Platforms",
+                newName: "OperatingSystemr");
+        }
+    }
+}
